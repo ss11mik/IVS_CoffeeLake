bc0d95b75c657e2956731a51dba9533941c58ed4
Your Name <jrh15248@tqosi.com>
2020-03-20 13:48:48 +0100

    build config

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,5 @@
 public class Main {
-    void run() {
+    public static void main(String [] args) {
         System.out.println("Hello World");
     }
 }
---
+++ src/main/java/MathLib.java
@@ -0,0 +1,6 @@
+public class MathLib {
+
+    public static double add (double a, double b) {
+        return a + b;
+    }
+}
