e74b2008bd054614e2af0053414c8656989729a1
Your Name <jrh15248@tqosi.com>
2020-03-20 13:52:44 +0100

    added basic functions to Mathlib

--- src/main/java/MathLib.java
+++ src/main/java/MathLib.java
@@ -3,4 +3,16 @@ public class MathLib {
     public static double add (double a, double b) {
         return a + b;
     }
+
+    public static double sub(double a, double b) {
+        return a - b;
+    }
+
+    public static double multiply (double a, double b) {
+        return a * b;
+    }
+
+    public static double divide (double a, double b) throws ArithmeticException {
+        return a / b;
+    }
 }
