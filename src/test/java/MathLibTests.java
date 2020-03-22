1197c9aa5432759adb4d576a267a5fb30ad764c9
MarkooCZ <m.lohn@seznam.cz>
2020-03-22 13:40:49 +0100

    Test update
    Added tests for sub()

--- src/test/java/MathLibTests.java
+++ src/test/java/MathLibTests.java
@@ -14,4 +14,15 @@ public class MathLibTests {
         assertEquals(0, MathLib.add(0,0), 0);
         assertEquals(9999999, MathLib.add(7777777,2222222), 0);
     }
+
+    @Test
+    public void testSub()
+    {
+        assertEquals(5, MathLib.sub(10,5), 0);
+        assertEquals(-2, MathLib.sub(7,9), 0);
+        assertEquals(16, MathLib.sub(7,-9), 0);
+        assertEquals(-16, MathLib.sub(-9,7), 0);
+        assertEquals(0, MathLib.sub(0,0), 0);
+        assertEquals(-9999999, MathLib.sub(-7777777,2222222), 0);
+    }
 }
