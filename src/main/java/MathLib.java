ce0492c781ec611c8c7f836e88fde7c281925ba9
ss11mik <ss11mik@protonmail.com>
2020-03-23 09:58:03 +0100

    MathLib done, except for sqrt. Tests also done, are passing.

--- src/main/java/MathLib.java
+++ src/main/java/MathLib.java
@@ -8,11 +8,36 @@ public class MathLib {
         return a - b;
     }
 
-    public static double multiply (double a, double b) {
+    public static double multiply(double a, double b) {
         return a * b;
     }
 
-    public static double divide (double a, double b) throws ArithmeticException {
+    public static double divide(double a, double b) throws ArithmeticException {
         return a / b;
+    }
+
+    public static double sqrt(double a, double b) {
+        //TODO use of Java Math permitted?
+        return 0;
+    }
+
+    public static long fact(long a) {
+        if(a <= 0)
+            return 1;
+        return fact(a-1) * a;
+    }
+
+    public static long pow(long a, long b) {
+        if (a == 0)
+            return 0;
+        long j = 1;
+        for (long i = 0; i < b; ++i) {
+            j *= a;
+        }
+        return j;
+    }
+
+    public static double mod(double a, double b) {
+        return a % b;
     }
 }
--- src/test/java/MathLibTests.java
+++ src/test/java/MathLibTests.java
@@ -4,25 +4,113 @@ import static org.junit.Assert.assertEquals;
 
 public class MathLibTests {
 
+    private static double TESTS_PRECISION = 0.00000001;
+
     @Test
-    public void testAdd()
-    {
+    public void testAdd() {
         assertEquals(7, MathLib.add(5,2), 0);
         assertEquals(-3, MathLib.add(5,-8), 0);
         assertEquals(-3, MathLib.add(-5,2), 0);
         assertEquals(2, MathLib.add(0,2), 0);
         assertEquals(0, MathLib.add(0,0), 0);
         assertEquals(9999999, MathLib.add(7777777,2222222), 0);
+
+        assertEquals(1.1, MathLib.add(1, 0.1), TESTS_PRECISION);
+        assertEquals(1.9, MathLib.add(1.8, 0.1), TESTS_PRECISION);
+        assertEquals(2.3, MathLib.add(0, 2.3), TESTS_PRECISION);
     }
 
     @Test
-    public void testSub()
-    {
+    public void testSub() {
         assertEquals(5, MathLib.sub(10,5), 0);
         assertEquals(-2, MathLib.sub(7,9), 0);
         assertEquals(16, MathLib.sub(7,-9), 0);
         assertEquals(-16, MathLib.sub(-9,7), 0);
         assertEquals(0, MathLib.sub(0,0), 0);
         assertEquals(-9999999, MathLib.sub(-7777777,2222222), 0);
+
+        assertEquals(-2.3, MathLib.sub(0, 2.3), TESTS_PRECISION);
+        assertEquals(2.3, MathLib.sub(0, -2.3), TESTS_PRECISION);
+        assertEquals(-12.3, MathLib.sub(-10, 2.3), TESTS_PRECISION);
+    }
+
+    @Test
+    public void testMultiply() {
+        assertEquals(50, MathLib.multiply(10,5), 0);
+        assertEquals(-50, MathLib.multiply(-10,5), 0);
+        assertEquals(-50, MathLib.multiply(10,-5), 0);
+        assertEquals(50, MathLib.multiply(-10,-5), 0);
+
+        assertEquals(0, MathLib.multiply(10,0), 0);
+        assertEquals(0, MathLib.multiply(-10,0), 0);
+        assertEquals(0, MathLib.multiply(0,0), 0);
+        assertEquals(0, MathLib.multiply(0.4,0), 0);
+        assertEquals(0, MathLib.multiply(69156541.785745,0), 0);
+
+        assertEquals(0.01, MathLib.multiply(0.1,0.1), TESTS_PRECISION);
+        assertEquals(1.0989, MathLib.multiply(1.1,0.999), TESTS_PRECISION);
+        assertEquals(399.6, MathLib.multiply(400,0.999), TESTS_PRECISION);
+        assertEquals(172839.160494, MathLib.multiply(222.222,777.777), TESTS_PRECISION);
+        assertEquals(-172839.160494, MathLib.multiply(-222.222,777.777), TESTS_PRECISION);
+    }
+
+    @Test
+    public void testDivide() {
+        assertEquals(2, MathLib.divide(10,5), 0);
+        assertEquals(-2, MathLib.divide(-10,5), 0);
+        assertEquals(-2, MathLib.divide(10,-5), 0);
+        assertEquals(2, MathLib.divide(-10,-5), 0);
+
+        assertEquals(0, MathLib.divide(0,5), 0);
+        assertEquals(0, MathLib.divide(0,-5), 0);
+
+        assertEquals(1, MathLib.divide(0.1,0.1), 0);
+        assertEquals(1.1011011011, MathLib.divide(1.1,0.999), TESTS_PRECISION);
+        assertEquals(64, MathLib.divide(65536,1024), 0);
+        assertEquals(0.285714285714, MathLib.divide(222.222,777.777), TESTS_PRECISION);
+        assertEquals(-0.285714285714, MathLib.divide(-222.222,777.777), TESTS_PRECISION);
+
+
+        assertEquals(Double.POSITIVE_INFINITY, MathLib.divide(1,0), 0);
+        assertEquals(Double.NEGATIVE_INFINITY, MathLib.divide(-10,0), 0);
+        assertEquals(Double.NaN, MathLib.divide(0,0), 0);
+    }
+
+    @Test
+    public void testFact() {
+        assertEquals(120, MathLib.fact(5));
+        assertEquals(1, MathLib.fact(0));
+        assertEquals(1, MathLib.fact(1));
+        assertEquals(1, MathLib.fact(-1));
+        assertEquals(479001600, MathLib.fact(12));
+    }
+
+    @Test
+    public void testPow() {
+        assertEquals(1, MathLib.pow(1, 0));
+        assertEquals(1, MathLib.pow(888, 0));
+        assertEquals(1, MathLib.pow(1, 888));
+        assertEquals(256, MathLib.pow(2, 8));
+        assertEquals(65536, MathLib.pow(2, 16));
+        assertEquals(0, MathLib.pow(0, 4096));
+        assertEquals(0, MathLib.pow(0, 0));
+    }
+
+    @Test
+    public void testMod() {
+        assertEquals(1, MathLib.mod(1, 8), 0);
+        assertEquals(1, MathLib.mod(9, 8), 0);
+        assertEquals(0, MathLib.mod(64, 32), 0);
+
+        assertEquals(0.1, MathLib.mod(1.1, 1), TESTS_PRECISION);
+        assertEquals(0, MathLib.mod(64.64, 32.32), TESTS_PRECISION);
+
+        assertEquals(0, MathLib.mod(64, -32), 0);
+        assertEquals(0, MathLib.mod(-64, 32), 0);
+        assertEquals(0, MathLib.mod(-64, -32), 0);
+
+        assertEquals(Double.NaN, MathLib.mod(1, 0), 0);
+        assertEquals(Double.NaN, MathLib.mod(0, 0), 0);
+        assertEquals(Double.NaN, MathLib.mod(-1, 0), 0);
     }
 }
