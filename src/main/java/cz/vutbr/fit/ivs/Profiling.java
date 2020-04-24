be808bb5bb45d9ff2cc55f6d7cf281c2949792a8
ss11mik <ss11mik@protonmail.com>
2020-04-24 11:28:23 +0200

    Added (standalone) class to profile MathLib.java

---
+++ src/main/java/cz/vutbr/fit/ivs/Profiling.java
@@ -0,0 +1,64 @@
+package cz.vutbr.fit.ivs;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Profiling {
+
+    public static void main(String[] args) {
+
+        List<Double> numbers = new ArrayList<Double>();
+
+        try {
+            // obtaining data
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            String str;
+
+            while ((str=br.readLine()) != null && str.length()!=0) {
+                numbers.add(Double.parseDouble(str));
+            }
+
+            // the calculation
+
+            double avg = average(numbers);
+            avg = MathLib.pow(avg, 2);
+            avg = MathLib.multiply(avg, numbers.size());    // N * avg^2
+
+
+            double part = 0;
+
+            for (double num : numbers) {
+                part = MathLib.add(part, MathLib.pow(num, 2));        // E x^2
+            }
+
+            part = MathLib.sub(part, avg);      // (E x^2 - N * avg^2)
+
+
+            double result =
+                    MathLib.sqrt(
+                            MathLib.multiply(
+                                    MathLib.divide(
+                                            1,
+                                            MathLib.sub(numbers.size(), 1)),
+                                    part),
+                            2);
+
+            System.out.println(result);
+
+        } catch (Exception e) {
+            System.err.println("Error:" + e.getMessage());
+        }
+    }
+
+    private static double average (List<Double> numbers) {
+        double avg = 0;
+        for (double num : numbers) {
+            avg += num;
+        }
+        avg /= numbers.size();
+        return avg;
+    }
+}
