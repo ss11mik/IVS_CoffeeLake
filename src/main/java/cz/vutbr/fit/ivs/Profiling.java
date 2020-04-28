00880e8bce1e180607eb4a583b1c6b7a04a6c4b4
ss11mik <ss11mik@protonmail.com>
2020-04-28 10:54:54 +0200

    Added header to dokumentace.tex and Javadoc to Profiling.java

--- src/main/java/cz/vutbr/fit/ivs/Profiling.java
+++ src/main/java/cz/vutbr/fit/ivs/Profiling.java
@@ -6,6 +6,12 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Independent class (not used in production) for profiling {@link MathLib} by computing variation
+ * Expects text file with one number in line as 1st arg
+ *
+ * @author Ondřej Mikula, René Szotkowski
+ */
 public class Profiling {
 
     public static void main(String[] args) {
